96e8e138f0135ae3005c44b2f9c3bb4c554baedd
Daniel Williams <danswilliams95@gmail.com>
2020-04-09 14:06:18 -0700

    Setting up Amplify.

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Unknown from './components/Unknown.js';
 
 const Routes = () => {
     return (
         <Switch>
             <Route path='/' exact component={Home} />
+            <Route exact component={Unknown} />
         </Switch>
     );
 };
---
+++ src/components/Unknown.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Unknown = () => {
+    return (
+        <h1>You've ventured off, page not found!</h1>
+    );
+};
+
+export default Unknown;
