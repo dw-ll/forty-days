226b88292712f3f3749a72682ecd0d33acf986fa
Daniel Williams <danswilliams95@gmail.com>
2020-04-12 16:44:09 -0700

    Rendering a user's notes works.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import TextLoop from 'react-text-loop';
-import '../styles/app.css';
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import TextLoop from "react-text-loop";
+import "../styles/app.css";
 
+const phrases = ["jot down how you're feeling,", "see how others are feeling,"];
+const Home = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  useEffect(() => {
+    async function onLoad() {
+      if (!props.authenticatedUser) {
+        return;
+      }
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        alert(e);
+      }
+      setIsloading(false);
+    }
+    onLoad();
+  }, [props.authenticatedUser]);
 
-
-const Home = () => {
-    const phrases = ["jot down how you're feeling,", "see how others are feeling,"]
-    return (
-        <div class="w-screen bg-gray-400 py-12">
-            <h1 class='text-2xl sm:text-4xl text-gray-900 font-semibold mt-6 px-8 sm:px-32'>Forty Days</h1>
-            <h2 class='text-md sm:text-lg px-8 sm:px-32'>A space to <TextLoop children={phrases}/> during the COVID-19 pandemic.</h2>
-            <div class='mt-4 px-8 sm:px-32'>
-                <a href='#' class='mt-3 inline-block px-5 py-3 rounded-lg bg-indigo-500 hvr:bg-indigo-800 shadow-lg text-white  tracking-wider font-semibold'>
-                    Get Started
-                </a>
-            </div>
-
-
-        </div>
+  const loadNotes = () => {
+    return API.get("notes", "/notes");
+  };
+  const renderNoteList = (notes) => {
+    return [{}].concat(notes).map((note, i) =>
+      i !== 0 ? (
+        <a href="#" class="block group hover:bg-gray-300 p-4 border-b w-full">
+          <p class="font-bold text-lg mb-1 text-black group-hover:text-white">
+            {note.title}
+          </p>
+          <p class="text-grey-darker mb-2 group-hover:text-white">
+            {note.content.trim().split("\n")[0]}
+          </p>
+        </a>
+      ) : (
+        <a href="/notes/new" class="block group hover:bg-gray-300 p-4 border-b">
+          <p class="font-bold text-lg mb-1 text-black group-hover:text-white">
+            <b>{"\uFF0B"}</b>
+            Create a new note
+          </p>
+        </a>
+      )
     );
+  };
+  const renderLanding = () => {
+    return (
+      <>
+        <div class="w-screen bg-gray-400 py-12">
+          <h1 class="text-2xl sm:text-4xl text-gray-900 font-semibold mt-6 px-8 sm:px-32">
+            Forty Days
+          </h1>
+          <h2 class="text-md sm:text-lg px-8 sm:px-32">
+            A space to <TextLoop children={phrases} /> during the COVID-19
+            pandemic.
+          </h2>
+          <div class="mt-4 px-8 sm:px-32">
+            <a
+              href="#"
+              class="mt-3 inline-block px-5 py-3 rounded-lg bg-gray-600 hover:bg-gray-800 shadow-lg text-white  tracking-wider font-semibold"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
+        <div class="md:w-1/2 h-full">
+          <h2 class="pl-2 md:pl-16 pt-10 md:pt-32 text-lg md:text-2xl lg:text-3xl font-bold">
+            Share how you're feeling, anonymously.
+          </h2>
+          <h2 class="pl-2 pr-5 md:pl-16 text-md md:text-xl text-gray-700">
+            We're experiencing an extremely isolating and digital time in our
+            lives.
+          </h2>
+          <h2 class="pl-2 md:pl-16 text-md md:text-xl text-gray-700">
+            Forty Days is a space to listen and talk to others by journaling,
+            without worrying about any sort of identity.
+          </h2>
+        </div>
+      </>
+    );
+  };
+  const renderNotes = () => {
+    return (
+      <div class="notes w-screen py-8 px-12">
+        <h1 class="sm:text-2xl lg:text-4xl w-full font-bold border-b-2">Your Notes</h1>
+        <div class="items-center justify-center w-full py-8">
+          <div class="overflow-hidden bg-white rounded w-full shadow-lg leading-normal">
+            {!isLoading && renderNoteList(notes)}
+          </div>
+        </div>
+      </div>
+    );
+  };
 
-}
+  return (
+    <div class="home">
+      {props.authenticatedUser ? renderNotes() : renderLanding()}
+    </div>
+  );
+};
 export default Home;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -17,7 +17,7 @@ const Login = (props) => {
     return fields.email.length > 0 && fields.password.length > 0;
   }
   async function handleSubmit(e) {
-    console.log('in submit');
+    console.log("in submit");
     e.preventDefault();
     setIsLoading(true);
     try {
@@ -30,57 +30,62 @@ const Login = (props) => {
     }
   }
   return (
-    <div class="login sm:flex sm:pt-24 sm:justify-center">
-      <div class="w-full max-w-xs">
-        <BlockUi tag="div" blocking={isLoading}>
-          <form
-            class="bg-white shadow-lg rounded px-8 pt-8 pb-6 mb-4"
-            onSubmit={handleSubmit}
-          >
-            <div class="mb-4">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="email"
-              >
-                Email Address
-              </label>
+    <div class="login pl-6 lg:pl-32 xl:pl-64 pt-24 lg:pt-32 xl:pt-48 justify-center bg-gray-400 h-screen">
+      <div class="lg:ml-8 xl:ml-10">
+        <h1 class="font-bold pl-10 lg:ml-16 xl:ml-20 md:pl-48 mb-6 text-2xl md:text-3xl">
+          Log in to Forty Days
+        </h1>
+        <div class="w-full lg:ml-32 xl:ml-64 content-center max-w-xs">
+          <BlockUi tag="div" blocking={isLoading}>
+            <form
+              class="bg-white shadow-lg rounded px-8 pt-8 pb-6 mb-4"
+              onSubmit={handleSubmit}
+            >
+              <div class="mb-4">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="email"
+                >
+                  Email Address
+                </label>
 
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                id='email'
-                placeholder="Email Address"
-                onChange={handleFieldChange}
-              />
-            </div>
-            <div class="mb-6">
-              <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="email"
-              >
-                Password
-              </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="email"
+                  placeholder="Email Address"
+                  onChange={handleFieldChange}
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="email"
+                >
+                  Password
+                </label>
 
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                id='password'
-                placeholder="Password"
-                onChange={handleFieldChange}
-              />
-            </div>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="password"
+                  placeholder="Password"
+                  onChange={handleFieldChange}
+                />
+              </div>
 
-            <div class="flex items-center justify-between">
-              <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
-                disabled={!validateSignIn()}
-                type="submit"
-              >
-                Sign In
-              </button>
-            </div>
-          </form>
-        </BlockUi>
+              <div class="flex items-center justify-between">
+                <button
+                  class="bg-gray-500 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none"
+                  disabled={!validateSignIn()}
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </BlockUi>
+        </div>
       </div>
     </div>
   );
